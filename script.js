49f1677cf049fe99fec1470d034e9e4b5a55347b
Cadenza <mirandalucsb@gmail.com>
2021-11-28 12:23:42 -0700

    added script file and comments, added ids to hours, added input to hours

---
+++ script.js
@@ -0,0 +1,7 @@
+//need to load the tasks from local storage into the elements.
+//need to save tasks to localstorage when the user presses the save button.
+//need to periodically run a function that checks what hour it is an whether or not tasks have passed.
+//add ability to change text in the long grey element when clicking on it
+//current day is displayed at the top of the calendar
+
+
